641f3c93922d84ade92e70a2cb1e4750b9dec7f0
chris-base <baseappsdev@gmail.com>
2022-07-29 19:25:28 -0500

    fixed timer type slider/color scss

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let currTimerType = 0;
+const themeColor1 = "#ff6257";
 
 document.getElementById("typePomo").addEventListener("click", function () {
   if (currTimerType !== 0) {
-    document.getElementById("typePomo").style.backgroundColor = "#c80a0a";
+    document.getElementById("typePomo").style.backgroundColor = themeColor1;
+    currTimerType = 0;
+    document.getElementById("typeShort").style.backgroundColor = "transparent";
+    document.getElementById("typeLong").style.backgroundColor = "transparent";
+  }
+});
+
+document.getElementById("typeShort").addEventListener("click", function () {
+  if (currTimerType !== 1) {
+    document.getElementById("typeShort").style.backgroundColor = themeColor1;
+    currTimerType = 1;
+    document.getElementById("typePomo").style.backgroundColor = "transparent";
+    document.getElementById("typeLong").style.backgroundColor = "transparent";
+  }
+});
+
+document.getElementById("typeLong").addEventListener("click", function () {
+  if (currTimerType !== 2) {
+    document.getElementById("typeLong").style.backgroundColor = themeColor1;
+    currTimerType = 2;
+    document.getElementById("typeShort").style.backgroundColor = "transparent";
+    document.getElementById("typePomo").style.backgroundColor = "transparent";
   }
 });
